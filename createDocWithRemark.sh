b371af283a59000480528ed783380d7880d521a7
Dirk Schumann <dirk@freunde-aus-berlin.net>
2020-10-06 22:07:46 +0200

    Script zur Erstellung der Doku mit Kommentaren

---
+++ createDocWithRemark.sh
@@ -0,0 +1,28 @@
+#!/bin/bash
+
+if [[ $# -ne 1 ]]; then
+    echo "Number of argument should be 1"
+    echo "Please use argument html, pdf, clean, all"
+    exit 1
+fi
+
+if [[ "$1" == "pdf" || "$1" == "html" ]]; 
+then
+    daps -d DC-Podlove_Podcast_Publisher_Dokumentation $1 --remarks
+    exit 0
+fi 
+
+if [[ "$1" == "all" ]];
+then
+    rm -r build
+    daps -d DC-Podlove_Podcast_Publisher_Dokumentation html --remarks
+    daps -d DC-Podlove_Podcast_Publisher_Dokumentation pdf --remarks
+    exit 0
+fi
+
+if [[ "$1" == "clean" ]];
+then
+    rm -r build
+else
+    echo "Please use argument html, pdf, clean or all"
+fi
